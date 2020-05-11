b4369369f8dc717ceb25850864763a9381e90b02
jripke74 <jripke74@gmail.com>
2020-05-11 12:01:49 -0700

    add object deconstructor

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -22,10 +22,13 @@ const renderMovies = (filter = '') => {
 
   filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
-    let text = movie.info.title + ' - ';
-    for (const key in movie.info) {
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    const { title: movieTitle } = info;
+    let text = movieTitle + ' - ';
+    for (const key in info) {
       if (key !== 'title') {
-        text = text + `${key}: ${movie.info[key]}`;
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
@@ -51,7 +54,7 @@ const addMovieHandler = () => {
       title,
       [extraName]: extraValue,
     },
-    id: Math.random(),
+    id: Math.random().toString()
   };
   movies.push(newMovie);
   renderMovies();
