e048cc45e4f48b10c87f7005556d48a771c7473b
jripke74 <jripke74@gmail.com>
2020-05-11 14:32:47 -0700

    using a method with this

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -25,8 +25,9 @@ const renderMovies = (filter = '') => {
     const { info, ...otherProps } = movie;
     console.log(otherProps);
     // const { title: movieTitle } = info;
-    // const { getFormattedTitle } = movie;
-    let text = movie.getFormattedTitle() + ' - ';
+    let { getFormattedTitle } = movie;
+    getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle() + ' - ';
     for (const key in info) {
       if (key !== 'title') {
         text = text + `${key}: ${info[key]}`;
